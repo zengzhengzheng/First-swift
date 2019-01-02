0186cb6568710c3bab2f018e2ba602d5c25a0761
zengzheng <zengzheng2009@gmail.com>
2019-01-03 01:32:46 +0800

    Initial Commit

---
+++ First-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First-swift
+//
+//  Created by zengzheng on 03/01/2019.
+//  Copyright © 2019 zengzheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
